d49c6a08d52ed181d7912a3ef64b35c70475e90b
tcinay <114522987+tcinay@users.noreply.github.com>
2025-11-18 16:27:07 +0000

    Deploying to gh-pages from @ tcinay/tcinay.github.io@2d12784a613d0800574947675224d66221c5f0c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,9 +46,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-galapagos-emissions-monitoring-station",
+          title: 'Galapagos Emissions Monitoring Station',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
